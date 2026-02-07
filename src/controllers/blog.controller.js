05e8533111af4694a20fd244ecefd968fe961096
foisaluddin400 <foisalrk2@gmail.com>
2026-02-07 21:03:07 +0600

    update category and subcategory

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -19,14 +19,40 @@ export const createBlog = asyncHandler(async (req, res) => {
 });
 
 // GET ALL BLOGS
+// GET ALL BLOGS with SEARCH + PAGINATION
 export const getBlogs = asyncHandler(async (req, res) => {
-  const blogs = await Blog.find();
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const search = req.query.search || "";
+
+  const skip = (page - 1) * limit;
+
+  // search condition
+  const searchQuery = search
+    ? {
+        $or: [
+          { title: { $regex: search, $options: "i" } },
+          { description: { $regex: search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const blogs = await Blog.find(searchQuery)
+    .skip(skip)
+    .limit(limit)
+    .sort({ createdAt: -1 });
+
+  const totalBlogs = await Blog.countDocuments(searchQuery);
 
   res.json({
     success: true,
+    currentPage: page,
+    totalPages: Math.ceil(totalBlogs / limit),
+    totalBlogs,
     data: blogs,
   });
 });
+
 
 // GET SINGLE BLOG
 export const getSingleBlog = asyncHandler(async (req, res) => {
